b4fe392880f95691fb3471ccfd45c1fc58847cb7
Petteri Mäntymaa <petteri.mantymaa@thl.fi>
2022-03-14 16:31:42 +0200

    explorations

--- explore/explore_oryx.R
+++ explore/explore_oryx.R
@@ -15,6 +15,14 @@ print_vehicle <- function(i) {
     "ul" = vehicle_ul)
 }
 
+split_vehicle <- function(string) {
+  n <- str_extract(string, "^([^\\s]*)")
+  name <- str_remove(string, "^([^\\s]*)\\s")
+  
+  data.frame("name" = name,
+             "n" = n)
+}
+
 library(rvest)
 library(xml2)
 library(tidyverse)
@@ -29,4 +37,42 @@ ru_header <- oryx %>%
 
 ru <- map_dfr(1:24, print_vehicle)
 
+ru.rep <- ru %>%
+  mutate(across(everything(), ~ str_replace_all(.x, "\\(.*?\\)", replacement = "")))
+
+ul_split <- str_split(ru.rep$ul, ":", simplify = TRUE) %>%
+  as.data.frame() %>%
+  mutate(across(everything(), .fns = ~ str_trim(.x, side = "both")))
+
+ru.rep.long <- ru.rep %>%
+  transmute(type = h3) %>%
+  cbind(ul_split) %>%
+  pivot_longer(cols = 2:23, names_to = "col", values_to = "ul") %>%
+  select(type, ul) %>%
+  filter(ul != "") %>%
+  cbind(split_vehicle(.$ul)) %>%
+  select(-ul) %>%
+  mutate(reportdate = Sys.Date(),
+         type = forcats::as_factor(type),
+         name = as.character(name),
+         n = as.numeric(n))
+
+file <- here::here(paste0("data/oryx_ru_losses_",Sys.Date(),".csv"))
+write.csv(ru.rep.long, file = file)
+
+ru.rep.long %>%
+  ggplot() +
+  geom_col(aes(x = type, y = n, fill = name)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        legend.position = "none")
+
+ru.rep.long %>%
+  summarise(sum = sum(n))
+
+ru.rep.long %>%
+  group_by(type) %>%
+  slice_max(order_by = n, n = 3) %>%
+  kable %>%
+  kable_styling()
 
